788b193ccb345b21b7bd00b661b63add4bb18d71
MuradWafik <muradalmakhzangi@gmail.com>
2024-06-26 17:22:04 +0300

    can search artists, sends a request with you input to spotify api returning artists names that match

--- src/script.js
+++ src/script.js
@@ -4,7 +4,6 @@ var jQueryScript = document.createElement('script');
 jQueryScript.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js');
 document.head.appendChild(jQueryScript);
 
-
 async function getToken() {
   const response = await fetch('https://accounts.spotify.com/api/token', {
     method: 'POST',
@@ -20,10 +19,14 @@ async function getToken() {
   return await response.json();
 }
 
-async function getArtist(token, artistName){
-    // const splitArtist = artistName.replace(" ", "+");
+// const token = await getToken().access_token;
+// const header = { Authorization: `Bearer ${token}` };
 
-    const result = await fetch("https://api.spotify.com/v1/search?q=Kendrick+Lamar&type=artist&market=US&offset=0", {
+
+async function getArtist(token, artistName, limit){
+    const splitArtist = artistName.replace(" ", "+");
+
+    const result = await fetch(`https://api.spotify.com/v1/search?q=${splitArtist}&type=artist&market=US&limit=${limit}&offset=0`, {
         method: "GET", headers: { Authorization: `Bearer ${token}` },
     });
 
@@ -31,12 +34,47 @@ async function getArtist(token, artistName){
 }
 
 getToken().then(response => {
-    getArtist(response.access_token, "Kendrick Lamar").then(artist => {
-      console.log(artist)
+    localStorage.setItem("token", response.access_token);
+    getArtist(response.access_token, "Kendrick Lamar", 1).then(artist => {
+      console.log(artist)  
     })
   });
 
+async function getAppearsOnTracks(token, limit, newArtistName, prevArtistName = null ){
+    // see what tracks the artist has appeared on to know if they are able to link
+}
 //  $(document).ready( function(){
 //     $("#submitTeamsButton").click(function(){
 //     });
 // });
+
+$(document).ready( function(){
+    $("#searchArtist")[0].addEventListener("input", () =>{
+        
+        let _artistName = $("#searchArtist").val();
+        const form = $("#parentForArtists");
+        form[0].textContent = "";
+        
+
+        if(_artistName.length == 0){
+            return;
+        }
+        getArtist(localStorage.getItem("token"), _artistName, 7).then(artist => {
+            // console.log(artist.artists.items)
+            // calls the get artist search function on spotify, with a limit of 7
+
+            const artistsFound = artist.artists.items // the object storing the actual individual artists
+            for (const _artist in artistsFound){
+
+                const thisArtist = artistsFound[_artist]
+                const artistListItem = document.createElement("div");
+                artistListItem.textContent = thisArtist.name;
+                form[0].appendChild(artistListItem);
+                // the individual index of each
+                console.log(thisArtist.name)
+
+            }
+          });
+
+    })
+});
